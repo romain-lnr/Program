6e9817d5a55dec4852f1b3682937d35f4b688ee7
Romain-len <89740581+romain-lnr@users.noreply.github.com>
2023-01-22 15:50:47 +0100

    Add Program

---
+++ C#/test/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace name {
+    class Program {
+    static void Main(string[] args) {
+        int a;
+        int b;
+        int result;
+        Addition_méthode(out a, out b, out result);
+        Console.WriteLine(a + " + " + b + " = " + result);
+    }
+    static void Addition_méthode(out int x, out int y, out int result) {
+        x = Convert.ToInt32(Console.ReadLine());
+        y = Convert.ToInt32(Console.ReadLine());
+        result = x + y;
+    }
+
+    }
+}
